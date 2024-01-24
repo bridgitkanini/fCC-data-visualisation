0fa906da439afed55073b37f5256fe1fd53f4dbe
bkm-code <144491053+bkm-code@users.noreply.github.com>
2024-01-24 11:14:53 +0300

    Scales abd Axes.

--- bar-chart.js
+++ bar-chart.js
@@ -4,7 +4,7 @@ let req = new XMLHttpRequest(); //Import JSON data.
 let data
 let values
 
-let heightScale
+let yScale
 let xScale
 let xAxis
 let yAxis
@@ -20,7 +20,34 @@ let drawCanvas = () => {
     svg.attr("height", height)
 };
 
-let generateScales = () => {};
+let generateScales = () => {
+
+    yScale = d3.scaleLinear()
+                .domain([0, d3.max(values, (item) => { //Lowest value obv 0, and funtion to get highest value; GDP.
+                    return item[1]
+                })])
+                .range([0, height - (2*padding)]);
+    
+    xScale = d3.scaleLinear()
+                .domain([0, values.length - 1])
+                .range([padding, width - padding]);
+
+    let datesArray = values.map((item) => { //Function to convert dates from strings to numeric dates.
+        return new Date(item[0])
+    });
+    
+    console.log(datesArray);
+                    
+    xAxis = d3.scaleTime()
+                .domain([d3.min(datesArray), d3.max(datesArray)])
+                .range([padding, width - padding]);
+
+    yAxis = d3.scaleLinear()
+                .domain([0, d.max(values, (item) => {
+                    return item[1]
+                })])
+                .range([height-padding, padding])
+};
 
 let drawBars = () => {};
 
