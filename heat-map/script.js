859b60aa44086fee3bc46572684320de3a457663
bkm-code <144491053+bkm-code@users.noreply.github.com>
2024-01-24 20:23:36 +0300

    Axes on Chart

--- heat-map/script.js
+++ heat-map/script.js
@@ -15,11 +15,33 @@ let canvas = d3.select("#canvas");
 canvas.attr("width", width);
 canvas.attr("height", height);
 
-let generateScales = () => {};
+let generateScales = () => {
+
+    xScale = d3.scaleLinear()
+                .range([padding, width - padding]);
+
+    yScale = d3.scaleTime()
+                .range([padding, height - padding]);
+};
 
 let drawCells = () => {};
 
-let drawAxes = () => {};
+let drawAxes = () => {
+
+    let xAxis = d3.axisBottom(xScale);
+
+    canvas.append("g")
+            .call(xAxis)
+            .attr("id", "x-axis")
+            .attr("transform", "translate(0, " + (height - padding) +")");
+
+    let yAxis = d3.axisLeft(yScale);
+
+    canvas.append("g")
+            .call(yAxis)
+            .attr("id", "y-axis")
+            .attr("transform", "translate(" + padding +", 0)");
+};
 
 req.open("GET", dataUrl, true); //Req methods to display data on live server console.
 req.onload = () => {
