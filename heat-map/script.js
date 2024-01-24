3814551b0d55b43835ae200b5de3d956ffd45121
bkm-code <144491053+bkm-code@users.noreply.github.com>
2024-01-24 19:25:35 +0300

    Create Variables and Functions

--- heat-map/script.js
+++ heat-map/script.js
@@ -1,1 +1,22 @@
-console.log(d3); //Check ✔.
+let dataUrl ="";
+let req = new XMLHttpRequest();
+
+let baseTemp
+let values
+
+let xScale
+let yScale
+
+let width = 1200;
+let height = 600;
+let padding = 60;
+
+let canvas = d3.select("#canvas");
+canvas.attr("width", width);
+canvas.attr("height", height);
+
+let generateScales = () => {};
+
+let drawCells = () => {};
+
+let drawAxes = () => {};
