62051049c9f5784a56538d36b0e0fb2a2ca2bd84
bkm-code <144491053+bkm-code@users.noreply.github.com>
2024-01-24 09:54:56 +0300

    Update

--- bar-chart.js
+++ bar-chart.js
@@ -1,1 +1,14 @@
-console.log(d3); //D3 methods confirmation on live server.
+let dataUrl = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+let req = new XMLHttpRequest(); //Import JSON data.
+
+let data
+let values
+
+let heightScale
+let xScale
+let xAxis
+let yAxis
+
+let width = 800;
+let height = 600;
+let padding = 40;
