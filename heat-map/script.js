e9de00aa831164d48766377cc9f4d64fd3739e71
bkm-code <144491053+bkm-code@users.noreply.github.com>
2024-01-25 09:29:44 +0300

    Update script.js

--- heat-map/script.js
+++ heat-map/script.js
@@ -7,6 +7,9 @@ let values
 let xScale
 let yScale
 
+let minYear
+let maxYear
+
 let width = 1200;
 let height = 600;
 let padding = 60;
@@ -17,10 +20,20 @@ canvas.attr("height", height);
 
 let generateScales = () => {
 
+    minYear = d3.min(values, (item) => {
+        return item["year"]
+    })
+
+    maxYear = d3.max(values, (item) => {
+        return item["year"]
+    })
+
     xScale = d3.scaleLinear()
+                .domain([minYear, maxYear]) //Can add +1 to maxYear bc data goes to 2015 but scale ends at 2000.
                 .range([padding, width - padding]);
 
     yScale = d3.scaleTime()
+                .domain([new Date(0, 0 , 0, 0, 0, 0), new Date(0, 12 , 0, 0, 0, 0)]) //12 not 11 for 12 gaps, one for each. Literature at https://www.w3schools.com/js/js_dates.asp  
                 .range([padding, height - padding]);
 };
 
@@ -55,11 +68,23 @@ let drawCells = () => {
             .attr("data-temp", (item) => {
                 return baseTemp + item["variance"]
             })
+            .attr("height", (height - (2*padding)) / 12)
+            .attr("y", (item) => {
+                return yScale(new Date(0, item["month"] - 1 , 0, 0, 0, 0)) //Aligning cells to the months, Naming the months appropriately, -1 to start from January..
+            })
+            .attr("width", (item) => {
+                let numberOfYears = maxYear - minYear;
+                return (width - (2*padding)) / numberOfYears  //Color coded cells should now display on the far left of the chart.
+            })
+            .attr("x", (item) => {
+                return xScale(item["year"])
+            });
 };
 
 let drawAxes = () => {
 
-    let xAxis = d3.axisBottom(xScale);
+    let xAxis = d3.axisBottom(xScale)
+                    .tickFormat(d3.format("d")); //"d" for decimal or integer; display years as integer without comma.
 
     canvas.append("g")
             .call(xAxis)
